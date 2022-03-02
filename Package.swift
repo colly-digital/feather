6d16795432cd74464deacedbac9c85dad4317888
Alex Tran Qui <alex.tranqui@asymtech.eu>
2022-03-02 09:53:31 +0100

    - WIP: incremental tunring on of modules to compile

--- Package.swift
+++ Package.swift
@@ -22,11 +22,11 @@ if isLocalDevMode {
 else {
     dependencies += [
         .package(url: "https://github.com/feathercms/feather-core", .revision("a54c9323416e5a45f7a2d9b4ee601444ff04f154")),
-//        .package(url: "https://github.com/colly-digital/analytics-module", .branch("colly")),
-//        .package(url: "https://github.com/colly-digital/aggregator-module", .branch("colly")),
+        .package(url: "https://github.com/colly-digital/analytics-module", .branch("colly")),
+        .package(url: "https://github.com/colly-digital/aggregator-module", .branch("colly")),
         .package(url: "https://github.com/colly-digital/blog-module", .branch("colly")),
         .package(url: "https://github.com/colly-digital/markdown-module", .branch("colly")),
-//        .package(url: "https://github.com/colly-digital/redirect-module", .branch("colly")),
+        .package(url: "https://github.com/colly-digital/redirect-module", .branch("colly")),
         .package(url: "https://github.com/colly-digital/swifty-module", .branch("colly")),
     ]
 }
@@ -47,7 +47,7 @@ let package = Package(
             .product(name: "LiquidLocalDriver", package: "liquid-local-driver"),
 //            .product(name: "AnalyticsModule", package: "analytics-module"),
 //            .product(name: "AggregatorModule", package: "aggregator-module"),
-            .product(name: "BlogModule", package: "blog-module"),
+//            .product(name: "BlogModule", package: "blog-module"),
 //            .product(name: "RedirectModule", package: "redirect-module"),
             .product(name: "SwiftyModule", package: "swifty-module"),
             .product(name: "MarkdownModule", package: "markdown-module"),
